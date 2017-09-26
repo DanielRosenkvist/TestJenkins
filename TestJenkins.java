355f303eece823ee6135be5f0901df3777170e6a
DanielRosenkvist <daniel.rosenkvist@sigmatechnology.se>
2017-09-26 08:25:30 +0200

    changed TestJenkins file

--- TestJenkins.java
+++ TestJenkins.java
@@ -3,5 +3,6 @@ public class TestJenkins {
 public static void main(String[] args) {
 	for(int i = 0; i < 10;i++)
 		System.out.println("Hello World! " + i );
+	System.out.println("Program ended...");
 }
 }
